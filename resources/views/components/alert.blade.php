6f20def677e29fedb38cf8cabde3b6e0a8802a4a
iarley <7iarley7@gmail.com>
2024-02-15 00:18:36 -0300

    Aula 37 - Feat: Estiliza alert de erro e cria novo component Success

--- resources/views/components/alert.blade.php
+++ resources/views/components/alert.blade.php
@@ -1,8 +1,8 @@
-<div class="alert-danger">
-    <!-- Simplicity is the consequence of refined emotions. - Jean D'Alembert -->
-    @if ($errors->any())
-        @foreach ($errors->all() as $error)
+<!-- Simplicity is the consequence of refined emotions. - Jean D'Alembert -->
+@if ($errors->any())
+    @foreach ($errors->all() as $error)
+        <div class="bg-orange-100 border-l-4 border-orange-500 text-orange-700 p-4 my-4" role="alert">
             <p><strong>{{ $error }}</strong></p>
-        @endforeach
-    @endif
-</div>
+        </div>
+    @endforeach
+@endif
